817dd715d4e156af6e45a7ff2e4a701c0a19a16d
mst <mst@touk.pl>
2016-01-05 21:16:17 +0100

    Removed index based min queue from project

--- mstvisualizer/src/main/java/org/gis/mstvisualizer/Core/Algorithms/PrimMST.java
+++ mstvisualizer/src/main/java/org/gis/mstvisualizer/Core/Algorithms/PrimMST.java
@@ -3,7 +3,6 @@ package org.gis.mstvisualizer.Core.Algorithms;
 import org.gis.mstvisualizer.Core.Graph.Edge;
 import org.gis.mstvisualizer.Core.Graph.MinEdgeComparator;
 import org.gis.mstvisualizer.Core.Graph.WeightedGraph;
-import org.gis.mstvisualizer.Core.IndexMinPriorityQueue;
 import org.gis.mstvisualizer.Core.Simulation.Events.AddEdgeToMstEvent;
 import org.gis.mstvisualizer.Core.Simulation.Events.VertexPickedEvent;
 import org.gis.mstvisualizer.Core.Simulation.Events.VertexVisitedEvent;
